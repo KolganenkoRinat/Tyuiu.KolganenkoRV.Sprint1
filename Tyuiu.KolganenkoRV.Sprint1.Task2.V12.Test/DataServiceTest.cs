e549f95fa221a6a7b53c1f4da465c46f40555b3e
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-09-25 22:08:11 +0500

    Task2

---
+++ Tyuiu.KolganenkoRV.Sprint1.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KolganenkoRV.Sprint1.Task2.V12.Lib;
+
+namespace Tyuiu.KolganenkoRV.Sprint1.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 2;
+            int z = 4;
+            var res = ds.CalculateParallelepipedVolume(x, y, z);
+            Assert.AreEqual(24, res);
+        }
+    }
+}
--- Tyuiu.KolganenkoRV.Sprint1.Task2.V12/Program.cs
+++ Tyuiu.KolganenkoRV.Sprint1.Task2.V12/Program.cs
@@ -1,6 +1,5 @@
-﻿using Tyuiu.KolganenkoRV.Sprint1.Task1.V2.Lib;
-
-namespace Tyuiu.KolganenkoRV.Sprint1.Task1.V2
+﻿using Tyuiu.KolganenkoRV.Sprint1.Task2.V12.Lib;
+namespace Tyuiu.KolganenkoRV.Sprint1.Task2.V12
 {
     class Program
     {
@@ -13,30 +12,37 @@ namespace Tyuiu.KolganenkoRV.Sprint1.Task1.V2
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-            Console.WriteLine("* Задание #1                                                              *");
-            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #12                                                              *");
             Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-            Console.WriteLine("* вычисляет результат по формуле x * y / (5 + x) и печатает его на экране.*");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
+
+            int x;
+            int y;
+            int z;
             
-            double x, y;
             Console.WriteLine("Введите значение X:");
-            x = Convert.ToDouble(Console.ReadLine());
+            x = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Введите значение Y:");
-            y = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Z:");
+            z = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine(ds.Calculate(x, y));
+            
+            Console.WriteLine("Объём параллелепипеда = " + ds.CalculateParallelepipedVolume(x, y, z));
             Console.ReadLine();
         }
     }
