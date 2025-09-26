a8709bdc815c246e1f273a4caf95b544636b9ceb
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-09-26 19:23:47 +0500

    Task3

---
+++ Tyuiu.KolganenkoRV.Sprint1.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KolganenkoRV.Sprint1.Task3.V7.Lib;
+
+namespace Tyuiu.KolganenkoRV.Sprint1.Task3.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            var res = ds.VerstsToKilometers(x);
+            Assert.AreEqual(2.133, res);
+        }
+    }
+}
---
+++ Tyuiu.KolganenkoRV.Sprint1.Task3.V7/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KolganenkoRV.Sprint1.Task3.V7.Lib;
+
+namespace Tyuiu.KolganenkoRV.Sprint1.Task3.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+         
+            Console.WriteLine("Введите расстояние в милях:");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Расстояние в километрах равно = " + ds.VerstsToKilometers(x));
+            Console.ReadLine();
+        }
+    }
+}
