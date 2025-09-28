6246b2ba32a6241b21b58b34bd7a053776f24d91
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-09-28 22:29:41 +0500

    Task6

---
+++ Tyuiu.KolganenkoRV.Sprint1.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Security;
+using Tyuiu.KolganenkoRV.Sprint1.Task6.V2.Lib;
+
+namespace Tyuiu.KolganenkoRV.Sprint1.Task6.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            string x = "Hello, world";
+            var res = ds.CheckHello(x);
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ Tyuiu.KolganenkoRV.Sprint1.Task6.V2/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KolganenkoRV.Sprint1.Task6.V2.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #2                                                              *");
+Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу: пользователь вводит текст.                          *");
+Console.WriteLine("* Проверить, есть и в строке слово Hello.                                *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите текст");
+string x = Console.ReadLine();
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.WriteLine(" есть ли слово Hello в строке? = " + ds.CheckHello(x));
+Console.ReadLine();
